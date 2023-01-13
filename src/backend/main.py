e0ea535ae4cb56d897904e8ea036635d3a0d61d3
dangwn <dangawne@outlook.com>
2023-01-13 08:16:35 +0000

    rm src

--- src/backend/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from app import app
-import config
-
-def main():
-    uvicorn.run(app, host = config.API_HOST_NAME, port = config.API_PORT)
-
-if __name__ == '__main__':
-    main()
