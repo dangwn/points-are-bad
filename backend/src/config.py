4c3c59a2e170258cd68a90a2bbe0456f9c2e957a
dangwn <dangawne@outlook.com>
2023-04-14 18:11:57 +0100

    new config

--- backend/src/config.py
+++ backend/src/config.py
@@ -19,14 +19,14 @@ DB_TYPE: str = os.getenv('DB_TYPE', 'postgresql+psycopg2')
 DB_USER: str = os.getenv('DB_USER', 'test')
 
 # Auth Config
-ACCESS_TOKEN_LIFETIME_MINUTES: int = 20
-ACCESS_TOKEN_SECRET: str = 'access_secret'
-REFRESH_TOKEN_LIFETIME_DAYS: int = 30
-REFRESH_TOKEN_SECRET: str = 'refresh_secret'
-REFRESH_TOKEN_COOKIE_KEY: str = 'X-Refresh-Token'
-CSRF_TOKEN_LIFETIME_DAYS: int = 30
-CSRF_TOKEN_SECRET: str = 'csrf_secret'
-CSRF_TOKEN_COOKIE_KEY: str = 'X-CSRF-Token'
+ACCESS_TOKEN_LIFETIME_MINUTES: int = int(os.getenv('ACCESS_TOKEN_LIFETIME_MINUTES', 20))
+ACCESS_TOKEN_SECRET: str = os.getenv('ACCESS_TOKEN_SECRET', 'access_secret')
+REFRESH_TOKEN_LIFETIME_DAYS: int = int(os.getenv('REFRESH_TOKEN_LIFETIME_DAYS', 30))
+REFRESH_TOKEN_SECRET: str = os.getenv('REFRESH_TOKEN_SECRET', 'refresh_secret')
+REFRESH_TOKEN_COOKIE_KEY: str = os.getenv('REFRESH_TOKEN_COOKIE_KEY', 'X-Refresh-Token')
+CSRF_TOKEN_LIFETIME_DAYS: int = int(os.getenv('CSRF_TOKEN_LIFETIME_DAYS', 30))
+CSRF_TOKEN_SECRET: str = os.getenv('CSRF_TOKEN_SECRET', 'csrf_secret')
+CSRF_TOKEN_COOKIE_KEY: str = os.getenv('CSRF_TOKEN_COOKIE_KEY', 'X-CSRF-Token')
 
 # Redis Config
 REDIS_HOST: str = os.getenv('REDIS_HOST', 'localhost')
