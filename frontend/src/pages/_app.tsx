ee90d37884df8bef2dd7390ad25452fc6c450bb1
dangwn <dangawne@outlook.com>
2023-04-20 08:25:47 +0100

    header

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from '../lib/queryClient';
 import CookiePopup from '../modules/cookie/CookiePopup';
@@ -8,6 +9,9 @@ import '../styles/global.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Points Are Bad</title>
+      </Head>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
