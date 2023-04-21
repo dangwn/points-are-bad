17646ff6e1ccf5355b412b1708cd5fae195a0be2
dangwn <dangawne@outlook.com>
2023-04-21 16:29:47 +0100

    new loading screen

--- frontend/src/modules/shared/Loading.tsx
+++ frontend/src/modules/shared/Loading.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import styles from '@/styles/shared/Loading.module.css'
+
 const Loading: React.FC = () => {
-  return <div>Loading...</div>
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.spinner}>
+        <div className={styles.spinnerDot}></div>
+        <div className={styles.spinnerDot}></div>
+        <div className={styles.spinnerDot}></div>
+        <div className={styles.spinnerDot}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
