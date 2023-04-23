0a4bd0401ac9219af7705ef51ab1c740d28957e9
dangwn <dangawne@outlook.com>
2023-04-23 18:00:43 +0100

    edit travis-deploy

--- CI/travis-deploy.sh
+++ CI/travis-deploy.sh
@@ -5,8 +5,8 @@ set -ex
 docker login --username "$DOCKERUSER" --password "$DOCKERPWD"
 
 # Build images
-docker build -t dangawne/points-are-bad-backend -f docker/Dockerfile_backend .
-docker build -t dangawne/points-are-bad-fleetcommand-agent -f docker/Dockerfile_fleetcommand_agent .
+docker build -t dangawne/points-are-bad-backend -f api-client/Dockerfile_backend .
+docker build -t dangawne/points-are-bad-fleetcommand-agent -f deploy/Dockerfile_fleetcommand_agent .
 
 # Push images
 docker push dangawne/points-are-bad-backend
