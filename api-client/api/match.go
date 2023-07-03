8b07c1a326569d7d7a1259c266547346b4a2eb2b
dangwn <dangawne@outlook.com>
2023-07-03 14:28:19 +0100

    match docs

--- api-client/api/match.go
+++ api-client/api/match.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -41,6 +41,7 @@ type MatchIdOnly struct {
  * Router Methods
  */
  
+// Match router group
 func (r Router) addMatchGroup(rg *gin.RouterGroup) {
     matchGroup := rg.Group("/match")
 
@@ -51,137 +52,151 @@ func (r Router) addMatchGroup(rg *gin.RouterGroup) {
 	matchGroup.GET("/full/", getFullMatches)
 }
 
+/*
+ * Match Creation Endpoint (Admin Only)
+ * Schema: {match_date: Date, home: string, away: string}
+ * Returns: MatchWithId
+ * Inserts a new match into DB with NULL home and away goals
+ */
 func createMatch(c *gin.Context) {
 	if !isCurrentUserAdmin(c) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"detail":"Not admin user",
-		})
+		abortRouterMethod(c, http.StatusUnauthorized, "Not admin user", "User cannot create match as they are not an admin user")
 		return
 	}
 
 	var match MatchWithoutGoals
 	if err := c.BindJSON(&match); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not retrieve data from request",
-		})
+		logMessage := "Could not bind incoming match data in createMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not retrieve data from request", logMessage)
 		return
 	}
 
-	if newMatch, err := insertMatchIntoDb(match); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not create match",
-		})
-	} else {
-		c.JSON(http.StatusCreated, newMatch)
+	newMatch, err := insertMatchIntoDb(match)
+	if err != nil {
+		logMessage := "Could not insert new match into DB in createMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not create match", logMessage)
+		return
 	}
+
+	c.JSON(http.StatusCreated, newMatch)
+	Logger.Info(fmt.Sprint("New match with ID ", newMatch.MatchId, " created"))
 }
 
+/*
+ * Match Deletion Endpoint (Admin Only)
+ * Query Params: match_id=int
+ * Deletes a match from the DB using a given match Id
+ */
 func deleteMatch(c *gin.Context) {
 	if !isCurrentUserAdmin(c) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"detail":"Not admin user",
-		})
+		abortRouterMethod(c, http.StatusUnauthorized, "Not admin user", "User cannot delete match as they are not an admin user")
 		return
 	}
 
 	var matchIdOnly MatchIdOnly
 	if err := c.BindQuery(&matchIdOnly); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not get match ID from request",
-		})
+		logMessage := "Could not get match Id from query in deleteMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not delete match", logMessage)
 		return
 	}
 
-	if deleted, _ := deleteMatchById(matchIdOnly.MatchId); !deleted {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not delete match",
-		})
+	if deleted, err := deleteMatchById(matchIdOnly.MatchId); !deleted {
+		logMessage := "Could not delete match from DB in deleteMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not delete match", logMessage)
 		return
 	}
+
 	c.Status(http.StatusNoContent)
+	Logger.Info(fmt.Sprint("Match ", matchIdOnly.MatchId, " deleted successfully"))
 }
 
+/*
+ * Full Matches Endpoint (Admin Only)
+ * Query Params: start_date=Date&end_date=Date
+ * Returns: []Match
+ * Retrieves all matches with all information, within a given date range
+ */
 func getFullMatches(c *gin.Context) {
 	if !isCurrentUserAdmin(c) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"detail":"Not admin user",
-		})
+		abortRouterMethod(c, http.StatusUnauthorized, "Not admin user", "User cannot get full matches as they are not an admin user")
 		return
 	}
 
 	var dateRange DateRange
 	if err := c.BindQuery(&dateRange); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Incorrect date formats",
-		})
+		logMessage := "Could not bind query in getFullMatches: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Match dates in incorrect format", logMessage)
 		return
 	}
 
 	matches, err := getFullMatchesInDateRange(dateRange.StartDate, dateRange.EndDate)
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not retrieve matches",
-		})
+		logMessage := "Could not get matches from DB in getFullMatches: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not retrieve matches", logMessage)
 		return
 	}
 
 	c.JSON(http.StatusAccepted, matches)
+	if userId, err := getCurrentUser(c); err == nil {
+		Logger.Info("Full matches successfully fetched for user " + userId)
+	} else {
+		Logger.Info("Full matches returned to unknown user")
+	}
 }
 
+/*
+ * Matches Without Goals Endpoint
+ * Query Params start_date=Date&end_date=Date
+ * Returns: []MatchWithoutGoals
+ * Retrieves all matches' match_dates, home and away teams, within a given date range
+ */
 func getMatchesWithoutGoals(c *gin.Context) {
 	var dateRange DateRange
 	if err := c.BindQuery(&dateRange); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Incorrect date formats",
-		})
+		logMessage := "Could not bind query in getMatchesWithoutGoals: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Match dates in incorrect format", logMessage)
 		return
 	}
 
 	matches, err := getMatchesInDateRange(dateRange.StartDate, dateRange.EndDate)
 	if err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not retrieve matches",
-		})
+		logMessage := "Could not get matches from DB in getMatchesWithoutGoals: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not retrieve matches", logMessage)
+		return
 	}
 
 	c.JSON(http.StatusAccepted, matches)
+	Logger.Info("Matches (without goals) successfully retrieved")
 }
 
+/*
+ * Update Match Endpoint
+ * Schema: {match_id: int, match_date: Date, home: string, away: string, home_goals: int|null, away_goals: int|null}
+ * Returns: Match
+ * Updates a match's data in DB, using a given match Id
+ */
 func updateMatch(c *gin.Context) {
 	if !isCurrentUserAdmin(c) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"detail":"Not admin user",
-		})
+		abortRouterMethod(c, http.StatusUnauthorized, "User is not admin user", "User cannot update match as they are not an admin user")
 		return
 	}
 
 	var match Match
 	if err := c.BindJSON(&match); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not get match ID from request",
-		})
+		logMessage := "Could not bind JSON in updateMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not get match from request", logMessage)
 		return
 	}
 
 	if newMatch, err := updateMatchById(match); err != nil {
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not update match",
-		})
+		logMessage := "Caught error when updating match in updateMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not update match", logMessage)
 	} else if !newMatch {
-		log.Println("could not update match")
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"detail":"Could not find match to update",
-		})
+		logMessage := "No rows affected when updating match in updateMatch: " + err.Error()
+		abortRouterMethod(c, http.StatusBadRequest, "Could not update match", logMessage)
 	} else {
 		c.JSON(http.StatusAccepted, match)
+		Logger.Info(fmt.Sprint("Match with Id ", match.MatchId, " updated successfully"))
 	}
 }
 
@@ -189,7 +204,12 @@ func updateMatch(c *gin.Context) {
  * Services
  */
 
-func deleteMatchById(matchId int) (bool, error) {
+/*
+ * Deletes a match with a given Id in the DB
+ * Only returns true when the number of rows affected is equal to 1
+ * Otherwise it will return false even if the error is nil
+ */
+ func deleteMatchById(matchId int) (bool, error) {
 	deleteQuery := "DELETE FROM matches WHERE match_id = $1"
 	if result, err := driver.Exec(deleteQuery, matchId); err != nil {
 		return false, err
@@ -202,6 +222,7 @@ func deleteMatchById(matchId int) (bool, error) {
 	}
 }
 
+// Gets []Match from the DB where startDate <= match_date < endDate
 func getFullMatchesInDateRange(startDate *Date, endDate *Date) ([]Match, error) {
 	var matches []Match
 
@@ -229,6 +250,7 @@ func getFullMatchesInDateRange(startDate *Date, endDate *Date) ([]Match, error) 
 	return matches, nil
 }
 
+// Gets []MatchWithoutGoals from the DB where startDate <= match_date < endDate
 func getMatchesInDateRange(startDate *Date, endDate *Date) ([]MatchWithoutGoals, error) {
 	var matches []MatchWithoutGoals
 
@@ -253,7 +275,11 @@ func getMatchesInDateRange(startDate *Date, endDate *Date) ([]MatchWithoutGoals,
 	return matches, nil	
 }
 
-func insertMatchIntoDb(match MatchWithoutGoals) (MatchWithId, error) {
+/*
+ * Inserts a match into the DB
+ * If any errors are caught, an empty MatchWithId is returned
+ */
+ func insertMatchIntoDb(match MatchWithoutGoals) (MatchWithId, error) {
 	var matchId int
 
 	insertQuery := `
@@ -280,6 +306,12 @@ func insertMatchIntoDb(match MatchWithoutGoals) (MatchWithId, error) {
 	}, nil
 }
 
+/*
+ * Updates a match in the DB
+ * The match Id is supplied in the request and is used to update a particular match
+ * Only returns true when the number of rows affected is equal to 1
+ * Otherwise it will return false even if the error is nil
+ */
 func updateMatchById(match Match) (bool, error) {
 	if result, err := driver.Exec(
 		`UPDATE matches 
@@ -299,10 +331,9 @@ func updateMatchById(match Match) (bool, error) {
 	); err != nil {
 		return false, err
 	} else {
-		if affected, err := result.RowsAffected(); err != nil {
-			return false, err
+		if n, err := result.RowsAffected(); n == 1 {
+			return true, err
 		} else {
-			if affected > 0 {return true, nil}
 			return false, nil
 		}
 	}
