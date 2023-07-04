fe3294b4ea5b3a37d7d4b32ca50c14939ba544bd
dangwn <dangawne@outlook.com>
2023-07-04 10:13:44 +0100

    middleware

--- api-client/api/router.go
+++ api-client/api/router.go
@@ -1,29 +1,11 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 type Router struct {
 	router *gin.Engine
-}
-
-func corsMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", FRONTEND_DOMAIN)
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(http.StatusNoContent)
-            return
-        }
-
-        c.Next()
-    }
 }
 
 func NewRouter() Router {
