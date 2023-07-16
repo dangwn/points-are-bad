efcdc9f7d0182efa45f233e4fed2a31270ee50a9
dangwn <dangawne@outlook.com>
2023-07-16 17:43:20 +0100

    infra init

---
+++ infra/tf_utils/tf_utils.go
@@ -0,0 +1,76 @@
+package tf_utils
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+type StackData struct {
+    TerraformDir string
+    StackParams map[string]interface{}
+    SkipDestroy bool
+
+    StackOutputs map[string]interface{}
+    
+    Options *terraform.Options
+}
+
+func (sd *StackData) configureOptions() {
+    if sd.Options == nil {
+           sd.Options = &terraform.Options{
+            TerraformDir:   sd.TerraformDir,
+            Vars:           sd.StackParams,
+        }
+    }
+}
+
+func (sd StackData) DeployStack(t *testing.T) {
+    sd.configureOptions()
+    terraform.InitAndApply(t, sd.Options)
+}
+
+func (sd *StackData) CollectOutputs(t *testing.T) {
+    sd.configureOptions()
+    outputs := terraform.OutputAll(t, sd.Options)
+    sd.StackOutputs = outputs
+}
+
+func (sd StackData) DestroyStack(t *testing.T) {
+    if !sd.SkipDestroy {
+        sd.configureOptions()
+        terraform.Destroy(t, sd.Options)
+    }
+}
+
+func (sd StackData) TearDown(t *testing.T, skipDestroy, skipCleanup bool, rootDir string) {
+    if !skipDestroy {
+        sd.DestroyStack(t)
+    }
+    if !skipCleanup {
+        RemoveTerraformStaticFiles(rootDir)
+        RemoveTFCacheDir(rootDir)
+    }
+}
+
+func RemoveTFCacheDir(rootDir string) {
+    tfCachePath := rootDir + "/.terraform"
+    if err := os.RemoveAll(tfCachePath); err != nil {
+        fmt.Println(err)
+    }
+}
+
+func RemoveTerraformStaticFiles(rootDir string) {
+    var tfStateFiles [3]string
+	tfStateFiles[0] = rootDir + "/terraform.tfstate.backup"
+	tfStateFiles[1] = rootDir + "/terraform.tfstate"
+	tfStateFiles[2] = rootDir + "/.terraform.lock.hcl"
+	for _, f := range tfStateFiles {
+		if err := os.Remove("./" + f); err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+	}
+}
